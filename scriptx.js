516eb4b2d82e7a15ad73b07092c4d4c58ebfc39d
keithg778 <79304253+keithg778@users.noreply.github.com>
2025-05-04 13:00:36 +0100

    Update scriptx.js

--- scriptx.js
+++ scriptx.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
+
+// code fragment
+// the form id is myForm
+$('#myForm').on('submit', function(event) {
+    event.preventDefault(); // prevent reload
+    
+    var formData = new FormData(this);
+    formData.append('service_id', 'service_fx90nh1');
+    formData.append('template_id', 'template_zaskzx5');
+    formData.append('user_id', 'RW1HRpw2mxpPmkVyK');
+
 const settings = {
   "async": true,
   "crossDomain": true,
@@ -13,9 +24,12 @@ const settings = {
   "data": "{\n  \"From\": \"mark.rogers@bentley.com\",\n  \"To\": \"keith.gunaratne@bentley.com\",\n  \"Subject\": \"Postmark test\",\n  \"TextBody\": \"Hello dear Postmark user.\",\n  \"HtmlBody\": \"<html><body><strong>Hello</strong> dear Postmark user.</body></html>\",\n  \"MessageStream\": \"outbound\"\n}"
 };
 
-$.ajax(settings).done(function() {
+    $.ajax(settings).done(function() {
         alert('Your mail is sent!');
     }).fail(function(error) {
         alert('Oops... ' + JSON.stringify(error));
     });
+});
+// code fragment
+
 });
